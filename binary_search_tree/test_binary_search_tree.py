5170ad072bebc5a10ce2d59b9b732d99506241cf
David Knupp <knupp@well.com>
2016-01-06 12:46:51 -0800

    Add test output to binary search tree unit test file.

--- binary_search_tree/test_binary_search_tree.py
+++ binary_search_tree/test_binary_search_tree.py
@@ -56,3 +56,30 @@ def test_tree_postorder_traversal(tree, capsys):
     tree.postorder()
     out, err = capsys.readouterr()
     assert out.split() == ['-2', '4', '0', '18', '15', '10']
+
+
+'''
+TEST OUTPUT
+$ py.test -sv --cov binary_search_tree.py test_binary_search_tree.py
+=================================================================================== test session starts ===================================================================================
+platform darwin -- Python 2.7.11, pytest-2.8.5, py-1.4.31, pluggy-0.3.1 -- /Users/dknupp/venvs/dev/bin/python2.7
+cachedir: .cache
+rootdir: /Users/dknupp/Documents/code/practice/binary_search_tree, inifile:
+plugins: cov-2.2.0
+collected 8 items
+
+test_binary_search_tree.py::test_tree_insert PASSED
+test_binary_search_tree.py::test_tree_find_smaller_than_root PASSED
+test_binary_search_tree.py::test_tree_find_larger_than_root PASSED
+test_binary_search_tree.py::test_empty_tree_find_fails PASSED
+test_binary_search_tree.py::test_tree_find_fails PASSED
+test_binary_search_tree.py::test_tree_inorder_traversal PASSED
+test_binary_search_tree.py::test_tree_preorder_traversal PASSED
+test_binary_search_tree.py::test_tree_postorder_traversal PASSED
+-------------------------------------------------------------------- coverage: platform darwin, python 2.7.11-final-0 ---------------------------------------------------------------------
+Name                    Stmts   Miss  Cover
+-------------------------------------------
+binary_search_tree.py      66      2    97%
+
+================================================================================ 8 passed in 0.02 seconds =================================================================================
+'''
